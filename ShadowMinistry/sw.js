b51e41a5fbdf40ef14c606bb7c528497d0ef83ab
sosaskze143 <sosaskze143@gmail.com>
2026-01-17 17:01:58 +0300

    Delete ShadowMinistry directory

--- ShadowMinistry/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Shadow Ministry SW Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // اتركها فارغة للسماح بمرور البيانات الحية من Render
-});
